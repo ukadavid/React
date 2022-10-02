f5aa3d744a6432cab7b0f6a7d5c9a6efd8eb0914
Tobechukwu David Uka <ukadavid7@gmail.com>
2022-10-02 15:56:43 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import ReactDom from 'react-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Card task
+function Card (props){
+ 
+}
+
+
+
+function App (){
+    const [count, setCount] = useState(0);
+    return(
+      <section>
+          <h1> count:{count}</h1>
+          <button onClick={() => setCount(count+1)}>Increment</button>
+          <button onClick={() => setCount(count-1)}>decrement</button>
+      </section>
+    )
 }
 
 export default App;
